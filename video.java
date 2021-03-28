80b8ece31b8f95a63ee267874b0d4d859d488d9a
leevisin <alwaysjoy233@163.com>
2021-03-28 19:44:21 +0800

    Nothing

--- video.java
+++ video.java
@@ -1,16 +1,26 @@
 /*
  * @Author: your name
  * @Date: 2021-03-28 17:26:56
- * @LastEditTime: 2021-03-28 17:48:17
+ * @LastEditTime: 2021-03-28 19:43:37
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \Video-Live\video.java
  */
 
- public class video{
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import java.io.*;
+import java.text.*;
 
-     public video(){
-         
+ public class Video extends JFrame implements ActionListener{
+
+     public Video(){
+         JPanel Jp1 = new JPanel();
+         JLabel Jl = new JLabel("Test");
+         Jp1.add(Jl);
+         getContentPane().add(Jp1);
      }
 
      public void searchVideo(){}
@@ -20,4 +30,14 @@
      public void removeVideo(){}
      
      public void clickVideo(){}
+
+
+     public static void main(String[] args) {
+        Video frame = new Video();
+        frame.setTitle("Videos V1.0");
+        frame.pack();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(600, 800);
+        frame.setVisible(true);
+     }
  }
