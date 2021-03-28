d932c13cf0598437152c4afba41ef89348a8c433
leevisin <alwaysjoy233@163.com>
2021-03-28 20:35:37 +0800

    Update AllCourse.java

--- AllCourse.java
+++ AllCourse.java
@@ -1,24 +1,18 @@
 /*
  * @Author: your name
  * @Date: 2021-03-28 20:02:49
- * @LastEditTime: 2021-03-28 20:18:17
+ * @LastEditTime: 2021-03-28 20:29:54
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \Video-Live\AllCourse.java
  */
-public class AllCourse {
+public class AllCourse extends Course{
 
     public AllCourse(){
          Course HIIT = new Course("HIIT", 30);
          Course Yoga = new Course("Yoga", 30);
          Course Strength = new Course("Strength", 30);
          Course Speed = new Course("Speed", 60);
-        System.out.println(HIIT.getCourseName());
-    }
-
-    public Course getCourse(String courseName){
-        if(courseName==Course.getCourseName())
-        return null;
     }
 
     
