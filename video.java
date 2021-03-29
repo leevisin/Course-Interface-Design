747a825ba57e224939935886db765b5333f324d2
leevisin <alwaysjoy233@163.com>
2021-03-29 12:02:35 +0800

    Implement file reader
    JButton 还是需要再弄，没办法单独对每个进行控制

--- video.java
+++ video.java
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-03-28 17:26:56
- * @LastEditTime: 2021-03-28 21:18:11
+ * @LastEditTime: 2021-03-29 12:01:34
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \Video-Live\video.java
@@ -16,29 +16,54 @@ import java.io.*;
      String fileName = "AllCourse.txt";
 
      public Video(){
-         JPanel mainPanel = new JPanel();
-         mainPanel.add(new JLabel("This is a Test for GUI"));
-         getContentPane().add(mainPanel);
-
-        System.out.println("We are in Constructor.");
+        JPanel mainPanel = new JPanel();
+        mainPanel.add(new JLabel("This is will be input search bar"));
+        // Get course information from file
         String fileContents = readFromFile(fileName);
-        System.out.println(fileContents);
-       
+        String[] splitFileContents = fileContents.split(" ");
+        int courseNum = splitFileContents.length/2;
+        
+        
+        // Temp store coure information indepently
+        String[] courseName = new String[courseNum];
+        int[] courseTime = new int[courseNum];
 
+        for(int i=0,j=0,k=0; i<splitFileContents.length; i++){
+            if(i%2==0){ courseName[j] = splitFileContents[i]; j++; }
+            else {courseTime[k] = Integer.parseInt(splitFileContents[i]); k++; }
+        }
+
+        // Generate JButton for each course
+        for(int i=0; i<courseNum; i++){
+            JButton jb = new JButton(courseName[i]);
+            jb.addActionListener(new ActionListener(){
+                public void actionPerformed(ActionEvent e){
+                    System.out.println("This button is clicked.");
+                    System.out.println("This course name is " + jb.getName() ); // return is still error.
+                }
+            });
+            mainPanel.add(jb);
+        }
+            
+        
+            
+       
+        getContentPane().add(mainPanel);
      }
 
-     public void actionPerformed(ActionEvent e){}
+    //  public void actionPerformed(ActionEvent e){
+    //      System.out.println("This button is clicked.");
+    //  }
 
      public String readFromFile(String filename){
-          // Read AllCourse.txt file
-        
+        // Read AllCourse.txt file
         try{
             String contents = "";
             FileReader fileReader = new FileReader(fileName);
             BufferedReader bufferedReader = new BufferedReader(fileReader);
             String oneLine = bufferedReader.readLine();
             while(oneLine != null){
-                contents += oneLine;
+                contents += oneLine + " ";
                 oneLine = bufferedReader.readLine();
             }
             bufferedReader.close();
@@ -49,7 +74,7 @@ import java.io.*;
             System.out.println("Errors occured: IOException");
             System.exit(1);
           }
-        return null; // When error occur.
+        return null; // When error occurs.
      }
      
      public void searchVideo(){}
