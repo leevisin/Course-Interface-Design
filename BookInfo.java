eb9c349b0223e70a04d70fecbc7c8e126fb2b47c
leevisin <alwaysjoy233@163.com>
2021-04-10 15:41:14 +0800

    Fix some places

--- BookInfo.java
+++ BookInfo.java
@@ -1,10 +1,18 @@
+/*
+ * @Author: your name
+ * @Date: 2021-03-31 21:40:58
+ * @LastEditTime: 2021-04-10 15:39:19
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \Video-Live\BookInfo.java
+ */
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.io.*;
 import java.text.*;
 
-public class BookInfo extends JFrame implements ActionListener{
+public class BookInfo extends JFrame{
 
     public BookInfo(String trainerName, String trainerType){
         JLabel info = new JLabel("You have booked " + trainerName + "'s " + trainerType + " course successfully!",JLabel.CENTER);
